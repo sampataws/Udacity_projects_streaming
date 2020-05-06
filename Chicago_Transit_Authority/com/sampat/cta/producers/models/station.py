67c5031beed9eae927e25a28096ce095b6081288
Sampat Kumar <sampat.budankayala@simpplr.com>
2020-05-07 00:12:38 +0530

    feat: Adding station.py which genrates a station event against a kafka topic
    The station.py inherits the base Producer class and generates Avro events by validating schemas and send a event to topic
    
    ""

--- Chicago_Transit_Authority/com/sampat/cta/producers/models/station.py
+++ Chicago_Transit_Authority/com/sampat/cta/producers/models/station.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 from confluent_kafka import avro
 
-from models import Turnstile
-from models.producer import Producer
+from Chicago_Transit_Authority.com.sampat.cta.producers.models import Turnstile
+from Chicago_Transit_Authority.com.sampat.cta.producers.models.producer import Producer
+from Chicago_Transit_Authority.com.sampat.cta.producers.models.utils import load_schema,format_station_name,RecordSchema
+from Chicago_Transit_Authority.com.sampat.cta.producers.models.topic_config import CtaTopics,join_topic_name
+
 
 
 logger = logging.getLogger(__name__)
@@ -14,38 +17,14 @@ logger = logging.getLogger(__name__)
 class Station(Producer):
     """Defines a single station"""
 
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
+    key_schema:RecordSchema = load_schema("arrival_key.json")
+    value_schema:RecordSchema = load_schema("arrival_value.json")
 
     #
     # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
     #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
-        self.name = name
-        station_name = (
-            self.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
-
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
-        super().__init__(
-            topic_name,
-            key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
-        )
-
         self.station_id = int(station_id)
         self.color = color
         self.dir_a = direction_a
@@ -54,6 +33,22 @@ class Station(Producer):
         self.b_train = None
         self.turnstile = Turnstile(self)
 
+        station_name = format_station_name(self.name)
+
+        #
+        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
+        # replicas
+        #
+        #
+        topic_name = join_topic_name(CtaTopics.ARRIVALS_PREFIX,station_name) # TODO: Come up with a better topic name
+        super().__init__(
+            topic_name,
+            key_schema=Station.key_schema,
+            value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
+            num_partitions=3,
+            num_replicas=1,
+        )
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
@@ -63,17 +58,20 @@ class Station(Producer):
         #
         #
         logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        self.producer.poll(0)
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id": self.station_id,
+                "train_id": train.train_id,
+                "direction": direction,
+                "line": self.color.name,
+                "train_status": train.status.name,
+                "prev_station_id": prev_station_id,
+                "prev_direction": prev_direction,
+            },
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
@@ -101,4 +99,5 @@ class Station(Producer):
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
         self.turnstile.close()
+        #super().close()
         super(Station, self).close()
