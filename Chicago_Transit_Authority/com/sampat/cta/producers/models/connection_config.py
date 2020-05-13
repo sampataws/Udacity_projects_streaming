ecb0ceeb3ccdb9b5dc8af6c0983cfd077d6077c4
Sampat Kumar <sampat.budankayala@simpplr.com>
2020-05-13 15:56:46 +0530

    refactor: Refactoring the URL path
    Adjust and refactoring the path URL to avoid unwanted spaces

--- Chicago_Transit_Authority/com/sampat/cta/producers/models/connection_config.py
+++ Chicago_Transit_Authority/com/sampat/cta/producers/models/connection_config.py
@@ -2,11 +2,11 @@ from typing import Dict
 
 
 class Connections:
-    KAFKA_BROKER: str = "PLAINTEXT://localhost:9092"
-    REST_PROXY: str = "http://localhost:8082"
-    SCHEMA_REGISTRY: str = "http://localhost:8081"
-    CONNECT: str = "http://localhost:8083"
-    KSQL: str = "http://localhost:8088"
+    KAFKA_BROKER: str = "PLAINTEXT://kafka0:9092"
+    REST_PROXY: str = "http://rest-proxy:8082"
+    SCHEMA_REGISTRY: str = "http://schema-registry:8081"
+    CONNECT: str = "http://kafka-connect:8083"
+    KSQL: str = "http://ksql:8088"
     POSTGRES: Dict[str, str] = {
         "connection.url": "jdbc:postgresql://postgres:5432/cta",
         "user": "cta_admin",
